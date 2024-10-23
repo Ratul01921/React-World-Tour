28d2ba0c09c4c728e14d3e2f39cb89ef669967fe
Shahriar Ratul <shahriarratul01921@gmail.com>
2024-10-23 17:35:32 +0600

    world name and flag show complete

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Countries from './components/countries/Countries'
+import Country from './components/Country/country'
+
+function App() {
+
+  return (
+    <>
+      
+      <h3>React World Tour</h3>
+      <Countries></Countries>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,15 @@
+import "./country.css"
+
+const Country = ({country}) => {
+    const {name, flags} = country;
+    console.log(name);
+    console.log(country);
+    return (
+        <div className="country">
+            <h3 >Name: {name?.common}</h3>
+            <img src={flags.png} alt="" />
+        </div>
+    );
+};
+
+export default Country;
